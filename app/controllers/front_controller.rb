5469c1d8e396fc4b90ff241407d74d93eaca81b9
stamander <iida.atsushi.fh@gmail.com>
2023-04-24 22:14:10 +0900

    Update front_controller.rb

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -1,5 +1,6 @@
 class FrontController < ApplicationController
   def index
+    redirect_to 'https://morokami-seminer.notion.site/morokami-seminer/MOROKAMI-SEMINER-96fe8116af224521b945956378b8c614'
   end
   
   def show
